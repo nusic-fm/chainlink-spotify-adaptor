e4628c5f4c92711f34d55bae72f68519c1753101
Logesh <55389513+logesh2496@users.noreply.github.com>
2021-11-26 16:54:37 +0530

    fix: port in app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const createRequest = require('./index').createRequest
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.EA_PORT || 8080
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.json())
 
